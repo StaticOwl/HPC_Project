de5ad8c47bd99fbf00247945ce8bba1d76813e2a
ayushiyad6 <46870959+ayushiyad6@users.noreply.github.com>
2024-04-21 23:17:23 -0400

    Added brute force method

--- tsp_module.py
+++ tsp_module.py
@@ -1,5 +1,7 @@
 # from or_tools.travelling_salesman_problem import optimize_routes
 from distance_api.distance_matrix import create_distance_matrix
+import itertools
+
 # from constants import api_key
 # api_key = "AIzaSyCL7xCEoletemwK_sqTHWDxtlhiCSm11jA"    # version 1
 
@@ -23,9 +25,35 @@ class RoutingOptimizer:
     def create_distance_matrix(self):
         self.distance_matrix = create_distance_matrix(self.addresses, self.api_key)
 
-    # def optimize_routes_tsp(self,):
-    #     results = optimize_routes(self.distance_matrix, self.depot)
-    #     return results
+
+    # helper function for brute force
+    def calculate_distance(self, city1, city2):
+    # Assuming city1 and city2 are tuples of (x, y) coordinates
+        return ((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2) ** 0.5
+
+    # helper function for brute force
+    def total_distance(self, path, distances):
+        total = 0
+        for i in range(len(path) - 1):
+            total += distances[path[i]][path[i+1]]
+        total += distances[path[-1]][path[0]]
+        return total
+
+    # brute force function - TSP
+    def brute_force_tsp(self, distance_matrix):
+        num_cities = len(distance_matrix)
+        cities = range(num_cities)
+        shortest_path = None
+        shortest_distance = float('inf')
+
+        for perm in itertools.permutations(cities):
+            distance = self.total_distance(perm, distance_matrix)
+            if distance < shortest_distance:
+                shortest_distance = distance
+                shortest_path = perm
+
+        return shortest_path, shortest_distance
+
 
 routing = RoutingOptimizer()
 # routing.add_address('Berlin+Germany')
@@ -39,3 +67,9 @@ routing.add_address('Saarland+Germany')
 print(routing.addresses)
 routing.create_distance_matrix()
 print(routing.distance_matrix)
+
+
+shortest_path, shortest_distance = routing.brute_force_tsp(routing.distance_matrix)
+print("Shortest path:", shortest_path)
+print("Shortest distance:", shortest_distance)
+print("hey")
